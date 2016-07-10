0b1ab6f65659a411383ce6198a5a4e078702f200
Traynor <traynordou@163.com>
2016-07-10 17:54:09 +0800

    new

--- script/USC-HA-User/main.go
+++ script/USC-HA-User/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "USC-HA-User/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.SetLogger("console", `{"level":7}`) // debug 级别以上的信息会被输出
+	beego.SetLogger("file", `{"filename":"logs/`+beego.AppConfig.String("appname")+`.log"}`)
 	beego.Run()
 }
-
